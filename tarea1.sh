229d46bad139414d51c3a0fe29c751015b827807
fernandorguezm <julfer@molsysbio.us.es>
2019-11-19 16:38:09 +0100

    continuamos tarea

--- tarea1.sh
+++ tarea1.sh
@@ -22,15 +22,27 @@ PARAMS=$1
 
 WD=$(grep working_directory: $PARAMS | awk '{ print $2 }')
 NUMSAM=$(grep number_of_samples: $PARAMS | awk '{ print $2 }')
+NUMCHIP=$(grep number_of_chip: $PARAMS | awk '{ print $2 }')
+NUMINPUT=$(grep number_of_input: $PARAMS | awk '{ print $2 }')
 GENOME=$(grep genome_accesion: $PARAMS | awk '{ print $2 }')
 ANNOTATION=$(grep annotation_accesion: $PARAMS | awk '{ print $2 }')
 
 SRACHIP=( )
 
 I=0
-while [ $I -lt $NUMSAM ]
+while [ $I -lt $NUMCHIP ]
 do
    SRACHIP[$I]=$(grep srachip$(($I + 1)): $PARAMS | awk '{ print $2 }')
+   ((I++))
+done
+
+
+SRAINPUT=( )
+
+I=0
+while [ $I -lt $NUMINPUT ]
+do
+   SRAINPUT[$I]=$(grep srainput$(($I + 1)): $PARAMS | awk '{ print $2 }')
    ((I++))
 done
 
@@ -43,11 +55,19 @@ echo GENOME=$GENOME
 echo ANNOTATION=$ANNOTATION
 
 I=0
-while [ $I -lt $NUMSAM ]
+while [ $I -lt $NUMCHIP ]
 do
    echo srachip$(($I+1))=${SRACHIP[$I]}
    ((I++))
 done
+
+I=0
+while [ $I -lt $NUMINPUT ]
+do
+   echo srainput$(($I+1))=${SRAINPUT[$I]}
+   ((I++))
+done
+
 
 
 ##Generate working directory
@@ -62,7 +82,7 @@ cd chip
 
 i=1
 
-while [ $i -le $NUMSAM ]
+while [ $i -le $NUMCHIP ]
 do
 mkdir sample$i
 ((i++))
@@ -72,7 +92,7 @@ cd ../input
 
 i=1
 
-while [ $i -le $NUMSAM ]
+while [ $i -le $NUMINPUT ]
 do
 mkdir sample$i
 ((i++))
@@ -99,12 +119,47 @@ cd $WD/samples/chip
 
 I=0
 
-while [ $I -lt $NUMSAM ]
+while [ $I -lt $NUMCHIP ]
 do
    cd $WD/samples/chip/sample$(($I + 1))
    fastq-dump --split-files ${SRACHIP[$I]}
    ((I++))
 done
 
+
+
+##Downaloadins samples input
+
+cd $WD/samples/input
+
+I=0
+
+while [ $I -lt $NUMINPUT ]
+do
+   cd $WD/samples/input/sample$(($I + 1))
+   fastq-dump --split-files ${SRAINPUT[$I]}
+   ((I++))
+done
+
+
+
+## Chip processing
+
+I=1
+while [ $I -le $NUMCHIP ]
+do
+   qsub -N chip$I -o $WD/logs/chip$I chip_processing.sh $I $WD $NUMCHIP ${SRACHIP[$I]}
+   ((I++))
+
+done
+
+## Input processing
+
+I=1
+while [ $I -le $NUMINPUT ]
+do
+   qsub -N input$I -o $WD/logs/input$I input_processing.sh $I $WD $NUMSAM ${SRAINPUT[$I]}
+   ((I++))
+done
 
 
