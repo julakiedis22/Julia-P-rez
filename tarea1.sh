eb0d593f563871c37a898d330fda40c36c2190af
fernandorguezm <julfer@molsysbio.us.es>
2019-11-20 16:39:10 +0100

    actualización con bucle para cp

--- tarea1.sh
+++ tarea1.sh
@@ -21,6 +21,7 @@ fi
 PARAMS=$1
 
 WD=$(grep working_directory: $PARAMS | awk '{ print $2 }')
+CF=$(grep copy_files: $PARAMS | awk '{ print $2 }')
 NUMSAM=$(grep number_of_samples: $PARAMS | awk '{ print $2 }')
 NUMCHIP=$(grep number_of_chip: $PARAMS | awk '{ print $2 }')
 NUMINPUT=$(grep number_of_input: $PARAMS | awk '{ print $2 }')
@@ -45,6 +46,26 @@ do
    SRAINPUT[$I]=$(grep srainput$(($I + 1)): $PARAMS | awk '{ print $2 }')
    ((I++))
 done
+
+FILESCHIP=( )
+
+I=0
+while [ $I -lt $NUMCHIP ]
+do
+   FILESCHIP[$I]=$(grep directorychip$(($I + 1)): $PARAMS | awk '{ print $2 }')
+   ((I++))
+done
+
+
+FILESINPUT=( )
+
+I=0
+while [ $I -lt $NUMINPUT ]
+do
+   FILESINPUT[$I]=$(grep directoryinput$(($I + 1)): $PARAMS | awk '{ print $2 }')
+   ((I++))
+done
+
 
 
 ##Debugging printing variable values
@@ -118,48 +139,106 @@ bowtie2-build genome.fa index
 cd $WD/samples/chip
 
 I=0
+J=0
 
-while [ $I -lt $NUMCHIP ]
-do
-   cd $WD/samples/chip/sample$(($I + 1))
-   fastq-dump --split-files ${SRACHIP[$I]}
-   ((I++))
-done
+if [ $CF -eq 1 ]
+then
+   while [ $I -lt $NUMCHIP ]
+   do
+      cp ${FILESCHIP[$I]} $WD/samples/chip/sample$(($I + 1))/sample$(($I + 1)).fastq
+      ((I++))
+   done
 
+elif [ $CF -eq 0 ]
+then
+   while [ $J -lt $NUMCHIP ]
+   do
+      cd $WD/samples/chip/sample$(($J + 1))
+      fastq-dump --split-files ${SRACHIP[$J]}
+      ((J++))
+   done
+else
+   echo "What"
+fi
 
 
 ##Downaloadins samples input
 
 cd $WD/samples/input
 
-I=0
 
-while [ $I -lt $NUMINPUT ]
-do
-   cd $WD/samples/input/sample$(($I + 1))
-   fastq-dump --split-files ${SRAINPUT[$I]}
-   ((I++))
-done
+I=0
+J=0
+
+if [ $CF -eq 1 ]
+then
+   while [ $I -lt $NUMINPUT ]
+   do
+      cp ${FILESINPUT[$I]} $WD/samples/input/sample$(($I + 1))/sample$(($I + 1)).fastq
+      ((I++))
+   done
+
+elif [ $CF -eq 0 ]
+then 
+   while [ $J -lt $NUMINPUT ]
+   do
+      cd $WD/samples/input/sample$(($J + 1))
+      fastq-dump --split-files ${SRAINPUT[$J]}
+      ((J++))
+   done
+else
+   echo "What"
+fi
 
 
 
 ## Chip processing
 
 I=1
-while [ $I -le $NUMCHIP ]
-do
-   qsub -N chip$I -o $WD/logs/chip$I chip_processing.sh $I $WD $NUMCHIP ${SRACHIP[$I]}
-   ((I++))
+J=1
 
-done
+if [ $CF -eq 1 ]
+then
+   while [ $I -le $NUMCHIP ]
+   do
+      qsub -N chip$I -o $WD/logs/chip$I /home/julfer/tareas/tarea1/Julia-P-rez/chip_processingprueba.sh $I $WD $NUMCHIP 0 $WD/samples/chip/sample$I/sample$I.fastq
+      ((I++))
+   done
+
+elif [ $CF -eq 0 ]
+then
+   while [ $J -lt $NUMCHIP ]
+   do
+      qsub -N chip$J -o $WD/logs/chip$J /home/julfer/tareas/tarea1/Julia-P-rez/chip_processing.sh $J $WD $NUMCHIP ${SRACHIP[$J]} 0
+      ((J++))
+   done
+else
+   echo "What"
+fi
+
 
 ## Input processing
 
 I=1
-while [ $I -le $NUMINPUT ]
-do
-   qsub -N input$I -o $WD/logs/input$I input_processing.sh $I $WD $NUMSAM ${SRAINPUT[$I]}
-   ((I++))
-done
+J=1
+
+if [ $CF -eq 1 ]
+then
+   while [ $I -le $NUMINPUT ]
+   do
+      qsub -N input$I -o $WD/logs/input$I /home/julfer/tareas/tarea1/Julia-P-rez/input_processingprueba.sh $I $WD $NUMINPUT 0 $WD/samples/input/sample$I/sample$I.fastq
+      ((I++))
+   done
+
+elif [ $CF -eq 0 ]
+then
+   while [ $J -lt $NUMINPUT ]
+   do
+      qsub -N input$J -o $WD/logs/input$J /home/julfer/tareas/tarea1/Julia-P-rez/input_processing.sh $J $WD $NUMINPUT ${SRAINPUT[$J]} 0
+      ((J++))
+   done
+else
+   echo "What"
+fi
 
 
